182f9a6f79634cf8aab1bab93480e2cdcb18ce33
Quang Trang <huynhquangtrang1994@gmail.com>
2021-10-23 23:46:05 +0700

    Add .NET Core backend API

---
+++ SimpleRegister/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRegister.Controllers
+{
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Index()
+        {
+            return Redirect("index.html");
+        }
+    }
+}
